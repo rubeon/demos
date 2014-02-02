8c56dae22c7f58abe769f7b760238c7f866019c4
srirajan <sriram.rajan@gmail.com>
2014-02-02 23:29:02 +0000

    reordered metadatae.rb

--- chef/site-cookbooks/roles_base/metadata.rb
+++ chef/site-cookbooks/roles_base/metadata.rb
@@ -6,19 +6,20 @@ description      'Installs/Configures roles_base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
+depends "remove_validation"
+
 depends "build-essential"
 depends "iptables"
 depends "base_pkgs"
+depends "sudo"
+depends "sudo_config"
 depends "users"
 depends "motd"
 depends "sshd"
-depends "sudo"
-depends "sudo_config"
-depends "logrotate"
-depends "logrotate_config"
 depends "cron"
 depends "cron_config"
+depends "logrotate"
+depends "logrotate_config"
 depends "sysctl"
 depends "sysctl_config"
-depends "remove_validation"
 
