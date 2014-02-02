6bc5738c63edb54d1617276c58ba7591d772b77a
srirajan <sriram.rajan@gmail.com>
2014-02-02 23:36:38 +0000

    fixed sshd and module issue

---
+++ chef/site-cookbooks/sshd/README.md
@@ -0,0 +1,20 @@
+sshd Cookbook
+=======================
+Configure sshd on the server
+
+
+Requirements
+------------
+
+
+Attributes
+----------
+
+Usage
+-----
+
+Contributing
+------------
+
+License and Authors
+-------------------
---
+++ chef/site-cookbooks/sshd/metadata.rb
@@ -0,0 +1,7 @@
+name             'sshd'
+maintainer       ''
+maintainer_email ''
+license          'All rights reserved'
+description      'Installs/Configures sshd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
