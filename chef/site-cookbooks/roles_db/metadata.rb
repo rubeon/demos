ff821651b19f485e9fd7fefed6bb99b93c05b426
srirajan <sriram.rajan@gmail.com>
2014-02-03 21:19:01 +0000

    updated metdata

--- chef/site-cookbooks/roles_db/metadata.rb
+++ chef/site-cookbooks/roles_db/metadata.rb
@@ -1,12 +1,13 @@
 name             'roles_db'
 maintainer       ''
-maintainer_email 'DevOpsAdvisory-INTL@RACKSPACE.COM'
+maintainer_email ''
 license          'Apache 2.0'
 description      'Installs/Configures roles_db'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.4'
 
 
 #### DB recipes
 depends "mysql"
 depends "mysql_config"
+depends "partial_search"
