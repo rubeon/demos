19d403b86d5bcd676f61e40dd0735c6733a9a023
srirajan <sriram.rajan@gmail.com>
2014-02-03 21:18:41 +0000

    fixed php attributes

--- chef/site-cookbooks/php_app/attributes/default.rb
+++ chef/site-cookbooks/php_app/attributes/default.rb
@@ -5,7 +5,7 @@ default["php_app"]["db_name"] = "world"
 default["php_app"]["db_pass_data_bag_item"] = "mysql_app"
 
 default["php_app"]["site_root"] = "/srv/www/"
-default["php_app"]["site_remote_url"] = "http://ca05e8171724524c805e-f85013b2b105db9295d244236b43548b.r11.cf3.rackcdn.com/one.example.com.tar.gz"
+default["php_app"]["site_remote_url"] = "https://github.com/bigcloudsolutions/demos/raw/master/demo-sites/one.example.com.tar.gz"
 default["php_app"]["site_remote_filename"] = "one.example.com.tar.gz"
 default["php_app"]["site_db_conf"]  = "/srv/www/one.example.com.db.php"
 
