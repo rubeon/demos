cd994f3a0a74a500db9e272493e263d081298512
srirajan <sriram.rajan@gmail.com>
2014-02-05 15:02:11 +0000

    commit

--- scripts/webui/PHPTail.php
+++ scripts/webui/PHPTail.php
@@ -233,18 +233,9 @@ class PHPTail {
 				</script>
 			</head> 
 			<body>
-				<div id="settings" title="PHPTail settings">
-					<p>Grep keyword (return results that contain this keyword)</p>
-					<input id="grep" type="text" value=""/>
-					<p>Should the grep keyword be inverted? (Return results that do NOT contain the keyword)</p>
-					<div id="invert">
-						<input type="radio" value="1" id="invert1" name="invert" /><label for="invert1">Yes</label>
-						<input type="radio" value="0" id="invert2" name="invert" checked="checked" /><label for="invert2">No</label>
-					</div>
-				</div>
 				<div class="float">
 					<button id="grepKeyword">Settings</button>
-					<span>Tailing file: <?php echo $this->log; ?></span> | <span id="grepspan">Grep keyword: ""</span> | <span id="invertspan">Inverted: false</span>
+					<span>Tailing file: <?php echo $this->log; ?></span>
 				</div>
 				<div id="results">
 				</div>
