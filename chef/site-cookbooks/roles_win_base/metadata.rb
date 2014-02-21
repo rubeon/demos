67049607beaba6b2757dacef1843c423edb716ec
srirajan <sriram.rajan@gmail.com>
2014-02-21 13:44:55 +0000

    adding cloudint examples

---
+++ chef/site-cookbooks/roles_win_base/metadata.rb
@@ -0,0 +1,9 @@
+name             'roles_win_base'
+maintainer       ''
+maintainer_email 'DevOpsAdvisory-INTL@RACKSPACE.COM'
+license          'Apache 2.0'
+description      'Installs/Configures roles_win_base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "windows"
