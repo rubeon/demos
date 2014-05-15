846f121c7bb7d817f74130073b130256a54f2244
srirajan <sriram.rajan@gmail.com>
2014-05-15 15:23:50 +0100

    removing windows bootstrap script. Will revise this later

--- win_scripts/bootstrap.cmd
+++
@@ -1,4 +0,0 @@
-net start w32time
-w32tm /config /manualpeerlist:"time.lon.rackspace.com 1.uk.pool.ntp.org 2.uk.pool.ntp.org 3.uk.pool.ntp.org" /syncfromflags:manual /reliable:yes /update
-w32tm /resync
-netsh advfirewall firewall set rule group="remote administration" new enable=yes & netsh advfirewall firewall add rule name="WinRM Port" dir=in action=allow protocol=TCP localport=5985
